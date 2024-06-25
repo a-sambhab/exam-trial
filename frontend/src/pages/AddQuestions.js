14ab77ed717139bec9a0f0e3132769eeaff10865
a-sambhab <saisambhav21chaini@gmail.com>
2024-06-25 12:59:47 +0530

    Patch

--- frontend/src/pages/AddQuestions.js
+++ frontend/src/pages/AddQuestions.js
@@ -7,8 +7,14 @@ const AddQuestions = () => {
   return (
     <div className="w-screen h-screen bg-[#E0FBFD] flex justify-evenly items-center">
       <Navbar user={user} />
-      <div className="w-[83%] h-[98%]">
-        
+      <div className="w-[83%] h-[98%] flex flex-col items-center justify-evenly">
+        <div className="w-[98%] h-[10%] text-3xl text-opacity-20 font-extrabold flex justify-start items-center opacity-70">
+            Create Test
+        </div>
+        <div className="w-[98%] h-[88%] rounded-lg bg-white flex justify-evenly items-center bg-opacity-50 border-2">
+            <div className="w-[48%] h-[98%] bg-white rounded-xl border-r-2"></div>
+            <div className="w-[48%] h-[98%]"></div>
+        </div>
       </div>
     </div>
   );
